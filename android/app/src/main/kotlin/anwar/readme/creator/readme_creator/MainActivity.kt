28d0bb3b6edee0e8e8898767da332b27d340550f
mhmdwaelanwr <mhmdwaelanwr@gmail.com>
2026-01-17 03:52:21 +0200

    Refactor codebase for conciseness and maintainability; add deep copy support to ReadmeElement types

--- android/app/src/main/kotlin/anwar/readme/creator/readme_creator/MainActivity.kt
+++ android/app/src/main/kotlin/anwar/readme/creator/readme_creator/MainActivity.kt
@@ -1,5 +1,19 @@
 package anwar.readme.creator.readme_creator
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import android.os.Build
+import android.view.View
+import android.view.WindowInsets
+import androidx.annotation.NonNull
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        
+        // Custom Android optimizations can be added here
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+        }
+    }
+}
